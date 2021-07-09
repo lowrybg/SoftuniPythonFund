7cf813abc7fd3c49495b9404c2a0c0d863a701ac
lowry <lowrybg@gmail.com>
2021-07-10 01:38:47 +0300

    training file added

--- training.py
+++ training.py
@@ -1,1 +1,15 @@
-nums = [1,2,3]
+import  math
+nums = input().split(', ')
+numbers = [int (n) for n in nums]
+
+max_num = max(numbers)
+cycle = math.ceil(max_num/10)
+x = 1
+
+while x < cycle+1:
+    ten_s = x*10
+
+    print(f'Group of {ten_s}\'s: {[int(num) for num in numbers if num<=ten_s and num > ten_s -10]}')
+    x+=1
+
+
