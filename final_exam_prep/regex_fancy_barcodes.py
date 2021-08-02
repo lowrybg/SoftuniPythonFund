5a245b696a322dcab6bb89a774c2dfae748881c3
Taner <lowrybg@users.noreply.github.com>
2021-08-02 12:39:31 +0300

    Final exam, few more tasks added

--- final_exam_prep/regex_fancy_barcodes.py
+++ final_exam_prep/regex_fancy_barcodes.py
@@ -1,27 +1,21 @@
 import re
-# TODO Working on 80%
+
 number = int(input())
 
 
-pattern_valid = r'(@(#)+)+(?P<important>[A-Z][A-Za-z0-9]{4,}[A-Z])\1'
+pattern_valid = r"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+"
 
 
 for _ in range(number):
     lines = input()
-    valid = re.search(pattern_valid,lines)
 
-    if valid:
-        valid = valid.group()
-        valid = str(valid)
-        product_group = ''
-        for n in range(len(valid)):
-            if valid[n].isdigit():
-                product_group += valid[n]
-        if product_group != '':
-            print(f'Product group: {product_group}')
+    if re.match(pattern_valid,lines):
+
+        digits = re.findall(r"\d", lines)
+
+        if digits:
+            print(f'Product group: {"".join(digits)}')
         else:
-            print(f'Product group: 00')
-        product_group = ''
+            print('Product group: 00')
     else:
-        print(f'Invalid barcode')
-
+        print('Invalid barcode')
